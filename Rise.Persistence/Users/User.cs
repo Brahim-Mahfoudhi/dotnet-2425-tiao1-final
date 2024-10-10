4f6fc6f91b201916441d306450779949be944053
FedDanPav <73825163+doorfed@users.noreply.github.com>
2024-10-10 18:36:12 +0200

    Add configurations for Address, Role and User

---
+++ Rise.Persistence/Users/User.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rise.Domain.Users;
+
+namespace Rise.Persistence.Users;
+
+internal class UserConfiguration : EntityConfiguration<User>
+{
+    public override void Configure(EntityTypeBuilder<User> builder)
+    {
+        base.Configure(builder);
+        builder.ToTable(nameof(User));
+
+        builder.HasIndex(x => x.Id).IsUnique();
+        
+        builder.Property(x => x.FirstName).IsRequired();
+        builder.Property(x => x.LastName).IsRequired();
+        builder.Property(x => x.Email).IsRequired();
+        builder.Property(x => x.Password).IsRequired();
+        builder.Property(x => x.BirthDate).IsRequired();
+        builder.Property(x => x.Address).IsRequired();
+        builder.Property(x => x.PhoneNumber).IsRequired();
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.UpdatedAt).IsRequired();
+
+        builder
+            .HasMany(x => x.Roles)
+            .WithMany();
+    }
+}
