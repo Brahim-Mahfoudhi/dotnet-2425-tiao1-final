82eaace80a6c6bdf6652dc2b7e24a2310c2caa23
Lorenz De Bie <lorenz.debie@student.hogent.be>
2024-10-13 22:39:40 +0200

    add registration validation

--- Rise.Shared/Users/UserDto.cs
+++ Rise.Shared/Users/UserDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Rise.Shared.Users;
 /// <summary>
 /// Data Transfer Object (DTO) representing a user with minimal info.
@@ -13,14 +15,18 @@ public class UserDto
         /// <summary>
         /// Gets or sets the first name of the user.
         /// </summary>
+        [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; } = default!;
         /// <summary>
         /// Gets or sets the last name of the user.
         /// </summary>
+        [Required(ErrorMessage = "Last name is required.")]
         public string LastName { get; set; } = default!;
         /// <summary>
         /// Gets or sets the email address of the user.
         /// </summary>
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public string Email { get; set; } = default!;
         /// <summary>
         /// Gets or sets the list of roles assigned to the user.
@@ -50,18 +56,22 @@ public class UserDto
         /// <summary>
         /// Gets or sets the password of the user.
         /// </summary>
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; } = default!;
         /// <summary>
         /// Gets or sets the birth date of the user.
         /// </summary>
+        [DateInThePast(ErrorMessage = "Birthdate must be a date in the past or today.")]
         public DateTime BirthDate { get; set; } = DateTime.Now;
         /// <summary>
         /// Gets or sets the address of the user.
         /// </summary>
-        public AddressDto.CreateAddress Address { get; set; } = new AddressDto.CreateAddress();
+        public AddressDto.CreateAddress Address { get; set; } = new();
         /// <summary>
         /// Gets or sets the phone number of the user.
         /// </summary>
+        [Required(ErrorMessage = "Phone number is required.")]
+        [BelgianPhoneNumber]
         public string PhoneNumber { get; set; } = default!;
     }
 
