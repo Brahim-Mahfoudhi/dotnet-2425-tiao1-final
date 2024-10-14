fb00763a606d86e33d061f6c6a50a4c14f4d7d8a
RobinBenoot2 <robin.benoot@student.hogent.be>
2024-10-14 22:30:20 +0200

    Bugfix
    Point to correct uri to resolve error

--- Rise.Services/Users/UserAuthService.cs
+++ Rise.Services/Users/UserAuthService.cs
@@ -8,9 +8,8 @@ namespace Rise.Services.Users;
 public class UserAuthService(HttpClient httpClient) : IUserAuthService
 {
     public async Task<IEnumerable<UserAuthDto.Index>> GetUsersAsync()
-    {
-        var users = await httpClient.GetFromJsonAsync<IEnumerable<UserAuthDto.Index>>("user");
-        Console.WriteLine(users);
+    { 
+        var users = await httpClient.GetFromJsonAsync<IEnumerable<UserAuthDto.Index>>("UserAuth");
         return users!;
     }
 }
