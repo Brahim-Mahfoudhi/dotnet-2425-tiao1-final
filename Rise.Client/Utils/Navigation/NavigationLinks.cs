5b84d09f3c02039d7a9888583f13c5c8a7412331
Glen Heyvaert <glen.heyvaert@gmail.com>
2024-10-23 10:36:47 +0200

    changed styling of main components

--- Rise.Client/Utils/Navigation/NavigationLinks.cs
+++ Rise.Client/Utils/Navigation/NavigationLinks.cs
@@ -5,11 +5,13 @@ public class NavigationLink
 {
     public string Url { get; set; }
     public string Name { get; set; }
+    public bool Authenticated { get; set; }
 
-    public NavigationLink(string url, string name)
+    public NavigationLink(string url, string name, bool authenticated)
     {
         Url = url;
         Name = name;
+        Authenticated = authenticated;
     }
 }
 
@@ -19,15 +21,19 @@ public class NavigationLinks
     private const string ACTUA = "/actua";
     private const string GOD_PARENT = "/meter-peter";
     private const string DOCUMENTS = "/documenten";
+    private const string USERS = "Users";
+    private const string AUTH_USERS = "authusers";
 
     public static List<NavigationLink> GetNavigationLinks()
     {
         return new List<NavigationLink>
         {
-            new (INFORMATION, "Information"),
-            new (ACTUA, "Actua"),
-            new (GOD_PARENT, "GodParents"),
-            new (DOCUMENTS, "Documents")
+            new (USERS, "Users", true),
+            new (AUTH_USERS, "Auth users", true),
+            new (INFORMATION, "Information", false),
+            new (ACTUA, "Actua", false),
+            new (GOD_PARENT, "GodParents", false),
+            new (DOCUMENTS, "Documents", false)
         };
     }
 }
